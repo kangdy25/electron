d5e6887ffb84cda5fac1a9d23bc897aa2f35aecf
Shineast <canfrog@naver.com>
2025-08-15 15:31:12 +0900

    Converting to ESModule except preload.js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js') // 'path.join' 대신 'join'을 사용합니다.
+      // path.join 대신 path.join을 사용하도록 수정
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   });
 
@@ -17,6 +24,7 @@ app.whenReady().then(() => {
   createWindow();
 
   app.on('activate', () => {
+    // macOS에서 Dock 아이콘을 클릭했을 때 창이 없으면 새 창을 생성합니다.
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
@@ -24,6 +32,7 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
+  // macOS가 아닌 다른 플랫폼에서는 모든 창이 닫히면 애플리케이션을 종료합니다.
   if (process.platform !== 'darwin') {
     app.quit();
   }
--- renderer.js
+++ renderer.js
@@ -1,2 +1,7 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+const information = document.getElementById('info');
+
+if (information) {
+  information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+} else {
+  console.log("HTML에 'info' ID를 가진 엘리먼트를 찾을 수 없습니다.");
+}
